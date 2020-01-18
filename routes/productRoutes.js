65de75fab6efb3c3b63c4bd3d09ff8f9c7dd328b
kagwi dru <kagwiandrew@gmail.com>
2020-01-18 22:19:26 +0300

    cleaned dead codes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,35 +5,11 @@ const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
-// router.param('id', productController.checkID);
-
-// POST /product/234fad4/reviews
-// GET /product/234fad4/reviews
-
 router.use('/:productId/reviews', reviewRouter);
 
 router
-  .route('/top-5-cheap')
+  .route('/top-cheap-bales')
   .get(productController.aliasTopProducts, productController.getAllProducts);
-
-router.route('/product-stats').get(productController.getProductStats);
-router
-  .route('/monthly-plan/:year')
-  .get(
-    authController.protect,
-    authController.restrictTo('admin'),
-    productController.getMonthlyPlan
-  );
-
-router
-  .route('/products-within/:distance/center/:latlng/unit/:unit')
-  .get(productController.getProductsWithin);
-// /products-within?distance=233&center=-40,45&unit=mi
-// /products-within/233/center/-40,45/unit/mi
-
-router
-  .route('/distances/:latlng/unit/:unit')
-  .get(productController.getDistances);
 
 router
   .route('/')
