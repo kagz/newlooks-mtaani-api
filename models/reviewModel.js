d71bb76be4d393bfd71dfc953034fdaa37ecb5f3
kagwi dru <kagwiandrew@gmail.com>
2020-01-23 02:04:11 +0300

    Update reviewModel.js

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -37,13 +37,13 @@ const reviewSchema = new mongoose.Schema(
 reviewSchema.index({ product: 1, user: 1 }, { unique: true });
 
 reviewSchema.pre(/^find/, function(next) {
-  // this.populate({
-  //   path: 'product',
-  //   select: 'name'
-  // }).populate({
-  //   path: 'user',
-  //   select: 'name photo'
-  // });
+  this.populate({
+    path: 'product',
+    select: 'name'
+  }).populate({
+    path: 'user',
+    select: 'name photo'
+  });
 
   this.populate({
     path: 'user',
